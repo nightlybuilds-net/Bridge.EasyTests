0191b9ce7751e4e901a0f504fc6cb9055fcb4045
Marco Milani <emme.milani@hotmail.it>
2018-01-05 17:52:47 +0100

    WIP

--- Bridge.EasyTests/Bridge.EasyTests/EasyAssert.cs
+++ Bridge.EasyTests/Bridge.EasyTests/EasyAssert.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Reflection;
+﻿using System.Reflection;
 
 namespace Bridge.EasyTests
 {
@@ -8,29 +6,5 @@ namespace Bridge.EasyTests
     {
         public string Description { get; set; }
         public bool Success { get; set; }
-    }
-
-    public class AssertResult
-    {
-        public  List<EasyAssert> Asserts { get; }
-
-        private AssertResult()
-        {
-            this.Asserts = new List<EasyAssert>();
-        }
-        
-        public static AssertResult Result => new AssertResult();
-
-        public AssertResult WithAssert(Func<bool> assert, string description)
-        {
-            this.Asserts.Add(new EasyAssert
-            {
-                Description = description,
-                Success = assert()
-            });
-            return this;
-        }
-
-        
     }
 }
